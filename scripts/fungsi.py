ae5671c03cad73f66e80d865916144f909bd5741
RadenDemas <radendemas21@gmail.com>
2024-11-15 18:10:34 +0700

    new file:   pages/index.html 	modified:   scripts/fungsi.py

--- scripts/fungsi.py
+++ scripts/fungsi.py
@@ -1,16 +1,24 @@
-import numpy
 
-def penjumlahan():
-    pass
+def penjumlahan(A,B):
+    if len(A) != len(B) or len(A[0]) != len(B[0]):
+        raise ValueError("Matriks harus memiliki ukuran yang sama.")
+    return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
 
-def pengurangan():
-    pass
+def pengurangan(A,B):
+    if len(A) != len(B) or len(A[0]) != len(B[0]):
+        raise ValueError("Matriks harus memiliki ukuran yang sama.")
+    return [[A[i][j] - B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
 
-def perkalian():
-    pass
+def perkalian(A,B):
+    if len(A[0]) != len(B):
+        raise ValueError("Jumlah kolom matriks A harus sama dengan jumlah baris matriks B.")
+    return [[sum(A[i][k] * B[k][j] for k in range(len(B))) for j in range(len(B[0]))] for i in range(len(A))]
 
-def inverse():
-    pass
 
-def transpose():
-    pass
+def inverse(A,B):
+        return [[A[j][i] for j in range(len(A))] for i in range(len(A[0]))]
+
+def transpose(A,B):
+    if len(A) != 2 or len(A[0]) != 2:
+        raise ValueError("Hanya mendukung matriks 2x2.")
+    return A[0][0] * A[1][1] - A[0][1] * A[1][0]
